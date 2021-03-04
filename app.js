a5996ab1f0282c8f1822e8724fb5168566fea603
Kurt Pessa <kpessa@gmail.com>
2021-03-04 10:35:47 -0500

    Added a Procfile.

--- app.js
+++ app.js
@@ -9,18 +9,33 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/notes', express.static(path.join(__dirname, 'public')));
 
+// ! --------------------------------------
+// !            WEBSITE SECTION
+// ! --------------------------------------
+// ****************************************
+// *         1. INDEX.HTML                *
+// ****************************************
 app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
-
+// ****************************************
+// *         2. NOTES.HTML                *
+// ****************************************
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
+// ! --------------------------------------
+// !              API SECTION
+// ! --------------------------------------
+// ****************************************
+// *  1.) GET / READ route for notes      *
+// ****************************************
 app.get('/api/notes', (req, res) => {
   res.json(JSON.parse(fs.readFileSync('./db/db.json') || []));
 });
-
+// ****************************************
+// *  2.) POST / CREATE route for notes   *
+// ****************************************
 app.post('/api/notes', (req, res) => {
   let notes = JSON.parse(fs.readFileSync('./db/db.json') || []);
   let newNote = {
@@ -31,17 +46,19 @@ app.post('/api/notes', (req, res) => {
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 });
-
+// ****************************************
+// * 3.) DELETE / DELETE route for notes  *
+// ****************************************
 app.delete('/api/notes/:id', (req, res) => {
   let notes = JSON.parse(fs.readFileSync('./db/db.json') || []);
   let noteIndex = notes.findIndex(note => note.id === +req.params.id);
   let note = notes.splice(noteIndex, 1);
-
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
-
   res.send(note);
 });
-
+// ! --------------------------------------
+// !     CATCH ALL - REDIRECT TO INDEX
+// ! --------------------------------------
 app.get('/:wildcard', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
